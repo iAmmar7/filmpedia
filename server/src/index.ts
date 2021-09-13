6537066e88b44ac26b1c988605e8cdec5e957212
Ammar Ansari <iammaransari@gmail.com>
2021-09-13 23:52:17 +0500

    server architecture updated

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,28 @@
+import { ApolloServer } from 'apollo-server-express';
+import cors from 'cors';
+import express from 'express';
 import 'dotenv/config';
 
 import '#root/db/connection';
-import startServer from '#root/server/startServer';
+import accessEnv from '#root/helpers/accessEnv';
+import resolvers from '#root/graphql/resolvers';
+import types from '#root/graphql/types';
 
-startServer();
+const PORT = accessEnv('PORT', 3000);
+
+(async function startServer() {
+  const apolloServer = new ApolloServer({ resolvers, typeDefs: types });
+
+  // Required logic for integrating with Express
+  await apolloServer.start();
+
+  const app = express();
+
+  app.use(cors());
+
+  apolloServer.applyMiddleware({ app, path: '/graphql' });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT} and GraphQL is on ${apolloServer.graphqlPath}`);
+  });
+})();
